5a347a404441701fd89625253aa8cdb48ae0249d
Minhajul Galib <migalibctg@gmail.com>
2023-09-25 03:41:49 +0600

    5ht sep

--- index.js
+++ index.js
@@ -10,11 +10,12 @@ app.use(cors());
 app.use(express.json());
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
+const objectId = require('mongodb').ObjectId;
+
+
 // const uri = `mongodb+srv://newDagencyBD:VzOvoxzGu7LZkbiv@cluster0.cs9gj.mongodb.net/?retryWrites=true&w=majority`;
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.cs9gj.mongodb.net/?retryWrites=true&w=majority`;
-console.log(process.env.DB_USER);
-console.log(process.env.DB_PASS);
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -32,6 +33,8 @@ async function run() {
     
         const databseCollection = client.db('Digital_Agency_BD');
         const userCollection = databseCollection.collection('user');
+        const coursesCollection = databseCollection.collection('Courses');
+        const servicesCollection = databseCollection.collection('Services');  
 
 
 // USER!
@@ -51,6 +54,44 @@ async function run() {
             res.send(allUser);
         });
 
+
+// COURSES!
+       // COURSES!
+       app.get('/courses', async (req, res)=>{
+        const courses = coursesCollection.find({});
+
+        const allCourses = await courses.toArray();
+        // console.log(objectId);
+
+            res.send(allCourses);
+        });
+
+        app.get('/courses/:id', async (req, res)=>{
+            const id = req.params.id;
+            const query = {_id: new objectId(id)};
+            const course = await coursesCollection.findOne(query);
+
+            res.json(course)
+        });
+
+
+// Services
+        // Services
+        app.get('/services', async (req, res)=>{
+            const services = servicesCollection.find({});
+
+            const allServices = await services.toArray();
+
+            res.send(allServices);
+        });
+
+        app.get('/services/:id', async (req, res)=>{
+            const id = req.params.id;
+            const query = {_id: new objectId(id)};
+            const service = await servicesCollection.findOne(query);
+            
+            res.json(service)
+        });
 
 
   } finally {
